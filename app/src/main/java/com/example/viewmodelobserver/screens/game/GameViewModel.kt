5bf233d629247f19ec56b01c1a8381045337d801
최훈엽 <>
2021-03-26 15:08:22 +0900

    Transformations.map()를 이용한 Live데이터 조작

--- app/src/main/java/com/example/viewmodelobserver/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/viewmodelobserver/screens/game/GameViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.viewmodelobserver.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -24,10 +44,27 @@ class GameViewModel: ViewModel() {
         get() = _eventGameFinish
 
     private lateinit var wordList: MutableList<String>
+    private val timer: CountDownTimer
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
     init {
         _word.value = ""
         _score.value = 0
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ONE_SECOND
+            }
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+
+        timer.start()
     }
     /**
      * Resets the list of words and randomizes the order
@@ -70,7 +107,7 @@ class GameViewModel: ViewModel() {
     private fun nextWord() {
         if (wordList.isEmpty()) {
             //Select and remove a word from the list
-            onGameFinish()
+            resetList()
         }else {
             _word.value = wordList.removeAt(0)
         }
@@ -97,5 +134,7 @@ class GameViewModel: ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
     }
+
 }

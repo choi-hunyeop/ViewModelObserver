047dcc7745e2880feefd5273158d6e1bc8e4d629
최훈엽 <>
2021-03-26 15:10:13 +0900

    Transformations.map(currentTime)

--- app/src/main/java/com/example/viewmodelobserver/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/viewmodelobserver/screens/game/GameViewModel.kt
@@ -1,11 +1,31 @@
 package com.example.viewmodelobserver.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+
+    companion object {
+
+        // Time when the game is over
+        private const val DONE = 0L
+
+        // Countdown time interval
+        private const val ONE_SECOND = 1000L
+
+        // Total time for the game
+        private const val COUNTDOWN_TIME = 60000L
+
+    }
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
 
     // The current word
     private val _word = MutableLiveData<String>()
